cf04332f5b4098ea9b7fba1f832b1338c0f82794
shadow-light <noreply@gracious.tech>
2022-12-30 17:47:22 +1100

    michael johnson

--- src/_comp/people.ts
+++ src/_comp/people.ts
@@ -18,6 +18,14 @@ export const people:Record<string, Person> = {
         bio: "Alan is the creator of the first New Testament critical text to use purely scientific textual criticism. He received his D.Litt. degree from the Kensington Theological Academy for his work in Greek New Testament textual criticism. Alan was previously an Assistant Professor in Computer Information Systems at Ivy Tech Community College and a Senior Lecturer in Computer Science at Purdue University. He has also authored several books and is an accomplished musician.",
         quote: "I want everyone to be able to access all of the biblical resources for free that I wish I would have had access to when I first became a Christian.",
     },
+    michael_johnson: {
+        name: "Michael Johnson",
+        title: "Director of eBible.org",
+        education: "",
+        site: "https://mljohnson.org/",
+        bio: "Michael distributes over 1,000 freely shareable Bible translations on eBible.org. He also served as a missionary in Papua New Guinea, and has a certificate in Bible translation from the Graduate Institute of Applied Linguistics.",
+        quote: "While starting to distribute the Holy Bible digitally, I ran into copyright restrictions that limited me to very old translations with archaic language. When I asked God what to do about that, the Lord directed me to do my own translation. With lots of help, the result is the copyright-free World English Bible. I started giving away Bible translation 'intellectual property' before it was popular to do so, and others found that made sense, to maximize ministry impact that way.",
+    },
     jon_here: {
         name: "Jon Here",
         title: "Founder of Gracious Tech",
