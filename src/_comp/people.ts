beb8d850b7b0aedf4f3536479cdefdd2bff5ae18
shadow-light <noreply@gracious.tech>
2023-01-02 11:46:35 +1100

    Shuffle

--- src/_comp/people.ts
+++ src/_comp/people.ts
@@ -26,14 +26,6 @@ export const people:Record<string, Person> = {
         bio: "Michael distributes over 1,000 freely shareable Bible translations on eBible.org. He also served as a missionary in Papua New Guinea, and has a certificate in Bible translation from the Graduate Institute of Applied Linguistics.",
         quote: "While starting to distribute the Holy Bible digitally, I ran into copyright restrictions that limited me to very old translations with archaic language. When I asked God what to do about that, the Lord directed me to do my own translation. With lots of help, the result is the copyright-free World English Bible. I started giving away Bible translation 'intellectual property' before it was popular to do so, and others found that made sense, to maximize ministry impact that way.",
     },
-    jon_here: {
-        name: "Jon Here",
-        title: "Founder of Gracious Tech",
-        education: "MDiv",
-        site: 'https://gracious.tech',
-        bio: "Jon has served as a pastor, a missionary in South-East Asia, and went on to start his own company for creating apps for mission. Every app his company makes is free to use and open source.",
-        quote: "The first app I made was for evangelizing using plain Scripture. It was almost done when I realised Bible translations forbid sharing plain Scripture! Copyright has been the number one barrier to my ministry ever since.",
-    },
     conley_owens: {
         name: "Conley Owens",
         title: "Author of The Dorean Principle",
@@ -49,5 +41,13 @@ export const people:Record<string, Person> = {
         site: 'https://hismagnificence.com/',
         bio: "Andrew is a Bible translation consultant and the cofounder of Aleph with Beth, which provides free videos for learning biblical Hebrew. He is also an author and musician.",
         quote: "I've seen up close the biblical knowledge famine in the developing world. I want my African brothers to have access to the same depth of Bible study that I have, but the status quo of copyright and monetized Christian resources keeps that from happening, and severely hinders my work in Bible translation. So I'm doing my part to encourage a reformation in this area.",
+    },
+    jon_here: {
+        name: "Jon Here",
+        title: "Founder of Gracious Tech",
+        education: "MDiv",
+        site: 'https://gracious.tech',
+        bio: "Jon has served as a pastor, a missionary in South-East Asia, and went on to start his own company for creating apps for mission. Every app his company makes is free to use and open source.",
+        quote: "The first app I made was for evangelizing using plain Scripture. It was almost done when I realised Bible translations forbid sharing plain Scripture! Copyright has been the number one barrier to my ministry ever since.",
     },
 }
