3e696d9381a720338efe34c56196f8ee6e81a6cc
shadow-light <noreply@gracious.tech>
2023-02-14 00:41:02 +1100

    Switch to new bible system

--- src/_comp/scripture.ts
+++ src/_comp/scripture.ts
@@ -8,6 +8,20 @@ export const scripture:Record<string, {net:string, esv:string, niv:string, csb:s
     csb: "“As you go, proclaim, ‘The kingdom of heaven has come near.’ Heal the sick, raise the dead, cleanse those with leprosy, drive out demons. Freely you received, freely give.”",
 },
 
+"Acts 4:32": {
+    net: "The group of those who believed were of one heart and mind, and no one said that any of his possessions was his own, but everything was held in common.",
+    esv: "Now the full number of those who believed were of one heart and soul, and no one said that any of the things that belonged to him was his own, but they had everything in common.",
+    niv: "All the believers were one in heart and mind. No one claimed that any of their possessions was their own, but they shared everything they had.",
+    csb: "Now the entire group of those who believed were of one heart and mind, and no one claimed that any of his possessions was his own, but instead they held everything in common.",
+},
+
+"1 Cor 6:7": {
+    net: "The fact that you have lawsuits among yourselves demonstrates that you have already been defeated. Why not rather be wronged? Why not rather be cheated?",
+    esv: "To have lawsuits at all with one another is already a defeat for you. Why not rather suffer wrong? Why not rather be defrauded?",
+    niv: "The very fact that you have lawsuits among you means you have been completely defeated already. Why not rather be wronged? Why not rather be cheated?",
+    csb: "As it is, to have legal disputes against one another is already a defeat for you. Why not rather be wronged? Why not rather be cheated?",
+},
+
 "1 Cor 8:9": {
     net: "But be careful that this liberty of yours does not become a hindrance to the weak.",
     esv: "But take care that this right of yours does not somehow become a stumbling block to the weak.",
@@ -20,6 +34,20 @@ export const scripture:Record<string, {net:string, esv:string, niv:string, csb:s
     esv: "This is my defense to those who would examine me. Do we not have the right to eat and drink? Do we not have the right to take along a believing wife, as do the other apostles and the brothers of the Lord and Cephas? Or is it only Barnabas and I who have no right to refrain from working for a living? Who serves as a soldier at his own expense? Who plants a vineyard without eating any of its fruit? Or who tends a flock without getting some of the milk?",
     niv: "This is my defense to those who sit in judgment on me. Don’t we have the right to food and drink? Don’t we have the right to take a believing wife along with us, as do the other apostles and the Lord’s brothers and Cephas? Or is it only I and Barnabas who lack the right to not work for a living? Who serves as a soldier at his own expense? Who plants a vineyard and does not eat its grapes? Who tends a flock and does not drink the milk?",
     csb: "My defense to those who examine me is this: Don’t we have the right to eat and drink? Don’t we have the right to be accompanied by a believing wife like the other apostles, the Lord’s brothers, and Cephas? Or do only Barnabas and I have no right to refrain from working? Who serves as a soldier at his own expense? Who plants a vineyard and does not eat its fruit? Or who shepherds a flock and does not drink the milk from the flock?",
+},
+
+"1 Cor 9:18": {
+    net: "What then is my reward? That when I preach the gospel I may offer the gospel free of charge, and so not make full use of my rights in the gospel.",
+    esv: "What then is my reward? That in my preaching I may present the gospel free of charge, so as not to make full use of my right in the gospel.",
+    niv: "What then is my reward? Just this: that in preaching the gospel I may offer it free of charge, and so not make full use of my rights as a preacher of the gospel.",
+    csb: "What then is my reward? To preach the gospel and offer it free of charge and not make full use of my rights in the gospel.",
+},
+
+"2 Cor 2:17": {
+    net: "For we are not like so many others, hucksters who peddle the word of God for profit, but we are speaking in Christ before God as persons of sincerity, as persons sent from God.",
+    esv: "For we are not, like so many, peddlers of God's word, but as men of sincerity, as commissioned by God, in the sight of God we speak in Christ.",
+    niv: "Unlike so many, we do not peddle the word of God for profit. On the contrary, in Christ we speak before God with sincerity, as those sent from God.",
+    csb: "For we do not market the word of God for profit like so many. On the contrary, we speak with sincerity in Christ, as from God and before God.",
 },
 
 "Rev 22:12": {
