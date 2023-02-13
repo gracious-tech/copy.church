257933e2407afee909548472cf4e93ba3c0d8c03
shadow-light <noreply@gracious.tech>
2023-02-13 11:44:18 +1100

    Site optional

--- src/_comp/CommittedPerson.vue
+++ src/_comp/CommittedPerson.vue
@@ -8,7 +8,7 @@ div.person
         div.cred
             h3 {{ person.name }}
             h4 {{ person.title }} #[span.edu {{ person.education }}]
-        a.website(:href='person.site' target='_blank' title="Website")
+        a.website(v-if='person.site' :href='person.site' target='_blank' title="Website")
             svg(viewBox='0 0 48 48')
                 path(d='M9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h13.95v3H9v30h30V25.05h3V39q0 1.2-.9 2.1-.9.9-2.1.9Zm10.1-10.95L17 28.9 36.9 9H25.95V6H42v16.05h-3v-10.9Z')
 
