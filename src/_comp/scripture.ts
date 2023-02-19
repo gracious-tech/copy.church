ca4a9d51a5a2cee96dc398efa757b9d8b639ada1
shadow-light <noreply@gracious.tech>
2023-02-20 08:31:36 +1100

    More passages

--- src/_comp/scripture.ts
+++ src/_comp/scripture.ts
@@ -1,11 +1,32 @@
 
 export const scripture:Record<string, {net:string, esv:string, niv:string, csb:string}> = {
 
+"Micah 3:11": {
+    net: "Her leaders take bribes when they decide legal cases, her priests proclaim rulings for profit, and her prophets read omens for pay. Yet they claim to trust the Lord and say, “The Lord is among us. Disaster will not overtake us!”",
+    esv: "Its heads give judgment for a bribe; its priests teach for a price; its prophets practice divination for money; yet they lean on the Lord and say, “Is not the Lord in the midst of us? No disaster shall come upon us.”",
+    niv: "Her leaders judge for a bribe, her priests teach for a price, and her prophets tell fortunes for money. Yet they look for the Lord’s support and say, “Is not the Lord among us? No disaster will come upon us.”",
+    csb: "Her leaders issue rulings for a bribe, her priests teach for payment, and her prophets practice divination for silver. Yet they lean on the Lord, saying, “Isn’t the Lord among us? No disaster will overtake us.”",
+},
+
+"Matt 9:37-38": {
+    net: "Then he said to his disciples, “The harvest is plentiful, but the workers are few. Therefore ask the Lord of the harvest to send out workers into his harvest-ready fields.”",
+    esv: "Then he said to his disciples, “The harvest is plentiful, but the laborers are few; therefore pray earnestly to the Lord of the harvest to send out laborers into his harvest.”",
+    niv: "Then he said to his disciples, “The harvest is plentiful but the workers are few. Ask the Lord of the harvest, therefore, to send out workers into his harvest field.”",
+    csb: "Then he said to his disciples, “The harvest is abundant, but the workers are few. Therefore, pray to the Lord of the harvest to send out workers into his harvest.”",
+},
+
 "Matt 10:7-8": {
     net: "“As you go, preach this message: ‘The kingdom of heaven is near!’ Heal the sick, raise the dead, cleanse lepers, cast out demons. Freely you received, freely give.”",
     esv: "“And proclaim as you go, saying, ‘The kingdom of heaven is at hand.’ Heal the sick, raise the dead, cleanse lepers, cast out demons. You received without paying; give without pay.”",
     niv: "“As you go, proclaim this message: ‘The kingdom of heaven has come near.’ Heal the sick, raise the dead, cleanse those who have leprosy, drive out demons. Freely you have received; freely give.”",
     csb: "“As you go, proclaim, ‘The kingdom of heaven has come near.’ Heal the sick, raise the dead, cleanse those with leprosy, drive out demons. Freely you received, freely give.”",
+},
+
+"Matt 10:9-10": {
+    net: "“Do not take gold, silver, or copper in your belts, no bag for the journey, or an extra tunic, or sandals or staff, for the worker deserves his provisions.”",
+    esv: "“Acquire no gold or silver or copper for your belts, no bag for your journey, or two tunics or sandals or a staff, for the laborer deserves his food.”",
+    niv: "“Do not get any gold or silver or copper to take with you in your belts— no bag for the journey or extra shirt or sandals or a staff, for the worker is worth his keep.”",
+    csb: "“Don’t acquire gold, silver, or copper for your money-belts. Don’t take a traveling bag for the road, or an extra shirt, sandals, or a staff, for the worker is worthy of his food.”",
 },
 
 "Acts 4:32": {
@@ -36,6 +57,13 @@ export const scripture:Record<string, {net:string, esv:string, niv:string, csb:s
     csb: "My defense to those who examine me is this: Don’t we have the right to eat and drink? Don’t we have the right to be accompanied by a believing wife like the other apostles, the Lord’s brothers, and Cephas? Or do only Barnabas and I have no right to refrain from working? Who serves as a soldier at his own expense? Who plants a vineyard and does not eat its fruit? Or who shepherds a flock and does not drink the milk from the flock?",
 },
 
+"1 Cor 9:13-14": {
+    net: "Don’t you know that those who serve in the temple eat food from the temple, and those who serve at the altar receive a part of the offerings? In the same way the Lord commanded those who proclaim the gospel to receive their living by the gospel.",
+    esv: "Do you not know that those who are employed in the temple service get their food from the temple, and those who serve at the altar share in the sacrificial offerings? In the same way, the Lord commanded that those who proclaim the gospel should get their living by the gospel.",
+    niv: "Don’t you know that those who serve in the temple get their food from the temple, and that those who serve at the altar share in what is offered on the altar? In the same way, the Lord has commanded that those who preach the gospel should receive their living from the gospel.",
+    csb: "Don’t you know that those who perform the temple services eat the food from the temple, and those who serve at the altar share in the offerings of the altar? In the same way, the Lord has commanded that those who preach the gospel should earn their living by the gospel.",
+},
+
 "1 Cor 9:18": {
     net: "What then is my reward? That when I preach the gospel I may offer the gospel free of charge, and so not make full use of my rights in the gospel.",
     esv: "What then is my reward? That in my preaching I may present the gospel free of charge, so as not to make full use of my right in the gospel.",
@@ -48,6 +76,13 @@ export const scripture:Record<string, {net:string, esv:string, niv:string, csb:s
     esv: "For we are not, like so many, peddlers of God's word, but as men of sincerity, as commissioned by God, in the sight of God we speak in Christ.",
     niv: "Unlike so many, we do not peddle the word of God for profit. On the contrary, in Christ we speak before God with sincerity, as those sent from God.",
     csb: "For we do not market the word of God for profit like so many. On the contrary, we speak with sincerity in Christ, as from God and before God.",
+},
+
+"Phil 4:18": {
+    net: "For I have received everything, and I have plenty. I have all I need because I received from Epaphroditus what you sent—a fragrant offering, an acceptable sacrifice, very pleasing to God.",
+    esv: "I have received full payment, and more. I am well supplied, having received from Epaphroditus the gifts you sent, a fragrant offering, a sacrifice acceptable and pleasing to God.",
+    niv: "I have received full payment and have more than enough. I am amply supplied, now that I have received from Epaphroditus the gifts you sent. They are a fragrant offering, an acceptable sacrifice, pleasing to God.",
+    csb: "But I have received everything in full, and I have an abundance. I am fully supplied, having received from Epaphroditus what you provided—a fragrant offering, an acceptable sacrifice, pleasing to God.",
 },
 
 "Rev 22:12": {
