12c52ac5648c45f4c9673e0cee8d4d08491f42ed
shadow-light <noreply@gracious.tech>
2023-03-10 00:03:48 +1100

    Router SSR fix

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -17,14 +17,22 @@ export default {
         ctx.app.component('BibleQuote', BibleQuote)
         ctx.app.component('CommittedPerson', CommittedPerson)
 
-        // Redirect if url is missing trailing slash, overwise VP will show a 404
+        // Custom route handling
+        // WARN Seems to break SSR but not needed for it anyway
         ctx.router.onAfterRouteChanged = to => {
-            const url = new URL(to)
-            if (!to.endsWith('/')){
-                ctx.router.go(to + '/')
-            } else if (url.pathname === '/overview/'){
-                // Overview page has been removed
-                ctx.router.go('/')
+            if (!import.meta.env.SSR){
+                // NOTE URL base probably not actually needed, just to be safe
+                const url = new URL(to, 'https://copy.church')
+                if (!to.endsWith('/')){
+                    // Redirect if url is missing trailing slash, overwise VP will show a 404
+                    ctx.router.go(to + '/')
+                } else if (url.pathname === '/overview/'){
+                    // Overview page has been removed
+                    ctx.router.go('/')
+                } else if (url.pathname === '/objections/'){
+                    // Objections index is under different path
+                    ctx.router.go('/explain/objections/')
+                }
             }
         }
     },
