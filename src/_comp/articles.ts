8b0f8a51c41fe7a055ae352d9aa5f417c121dccc
shadow-light <noreply@gracious.tech>
2023-03-22 21:40:27 +1100

    Article meta

--- src/_comp/articles.ts
+++ src/_comp/articles.ts
@@ -1,7 +1,7 @@
 
 // Articles here can be used in ArticlePreview and will auto-appear in sidebar and more depth page
 // NOTE The title of these should be simple and succinct, so may not match actual article's title
-export const articles = {
+export const articles:Record<string, {title:string, desc:string}> = {
     'command-many-overlook': {
         title: "The freely giving ethic",
         desc: "How Jesus' command to freely give works with Paul's teaching on financial support.",
@@ -13,5 +13,9 @@ export const articles = {
     'commerce-condemned': {
         title: "Condemnation of commercial ministry",
         desc: "There are many passages that directly condemn commercializing ministry.",
+    },
+    'selling-truth': {
+        title: "Examining Micah 3:11",
+        desc: "Where God clearly condemns charging for religious teaching.",
     },
 }
