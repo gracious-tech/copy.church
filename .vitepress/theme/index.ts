5669ac86f6d95c8e6c7a6e2f12ec5cbdbfc5af30
shadow-light <noreply@gracious.tech>
2024-04-02 09:59:50 +1100

    .

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 import VPButton from 'vitepress/dist/client/theme-default/components/VPButton.vue'
 import {Theme} from 'vitepress'
 
+import CustomLayout from './CustomLayout.vue'
 import BibleQuote from '@/_comp/BibleQuote.vue'
 import CommittedPerson from '@/_comp/CommittedPerson.vue'
 import ArticlePreview from '@/_comp/ArticlePreview.vue'
@@ -12,6 +13,7 @@ import './custom.sass'
 
 export default {
     ...DefaultTheme,
+    Layout: CustomLayout,
     enhanceApp(ctx){
         DefaultTheme.enhanceApp(ctx)
         // Global components
