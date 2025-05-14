5d2851f1851aa5b2a631d080f2b7012f4b50711c
shadow-light <noreply@gracious.tech>
2025-05-14 21:02:23 +1000

    pub date

--- src/_comp/petition/DisplaySigners.vue
+++ src/_comp/petition/DisplaySigners.vue
@@ -9,6 +9,7 @@ teleport(to='body')
 div.list(:class='{truncate}')
     //- NOTE Condition is so VitePress outline doesn't show incomplete number in heading
     h2#signers List of signers {{ total ? `(${total})` : "" }}
+    p Since publication in May 2025
     div.grid
         div(v-for='signer of public_signers')
             div.name
